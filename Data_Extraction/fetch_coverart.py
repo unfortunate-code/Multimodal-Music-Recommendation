a40aef74d4f48e2e2a6460d21aaf40a29c1eec41
unfortunate-code <alameluvarianirudh3@gmail.com>
2022-03-01 22:11:44 -0800

    Adding data extraction files

---
+++ Data_Extraction/fetch_coverart.py
@@ -0,0 +1,35 @@
+from lyricsgenius import Genius
+import sys
+
+ACCESS_TOKEN = 'efdoQZPNfD67-vtu0L77LV6XYjVg4OepeLPxsun2Y8Ferpxvq-be7a8osNEpkvxZ'
+
+def main():
+    genius = Genius(ACCESS_TOKEN)
+    genius.remove_section_headers = True
+    genius.excluded_terms = ["(Remix)", "(Live)"]
+    args = sys.argv[1:]
+    artist = None
+    songName = None
+    i = 0
+    while i < len(args):
+        if args[i] == '-a':
+            artist = args[i + 1]
+        elif args[i] == '-s':
+            songName = args[i + 1]
+        else:
+            print("%s is not a valid argument. Use -a for artist, -s for song", args)
+            i += 1
+            continue
+        i += 2
+    if songName != None and artist == None:
+        song = genius.search_song(songName)
+    elif songName != None and artist != None:
+        song = genius.search_song(songName, artist)
+    else:
+        print("Atleast song name needs to be passed. Use -s <song> -a <artist>")
+        return
+    song_dict = genius.song(song.id)
+    print("song_img: " + song_dict['song']['header_image_url'] + ", album_img: "  + song_dict['song']['song_art_image_url'] + ", song_description: " + song_dict['song']['description']['plain'])
+
+if __name__ == '__main__':
+    main()
---
+++ Data_Extraction/fetch_lyrics.py
@@ -0,0 +1,40 @@
+from lyricsgenius import Genius
+import sys
+
+ACCESS_TOKEN = 'efdoQZPNfD67-vtu0L77LV6XYjVg4OepeLPxsun2Y8Ferpxvq-be7a8osNEpkvxZ'
+
+def main():
+    genius = Genius(ACCESS_TOKEN)
+    genius.remove_section_headers = True
+    genius.excluded_terms = ["(Remix)", "(Live)"]
+    args = sys.argv[1:]
+    artist = None
+    songName = None
+    fileName = None
+    i = 0
+    while i < len(args):
+        if args[i] == '-a':
+            artist = args[i + 1]
+        elif args[i] == '-s':
+            songName = args[i + 1]
+        elif args[i] == '-f':
+            fileName = args[i + 1]
+        else:
+            print("%s is not a valid argument. Use -a for artist, -s for song and -f for filename you want to save the lyrics in when only artist is passed", args)
+            i += 1
+            continue
+        i += 2
+    if songName == None and artist != None:
+        artist = genius.search_artist(artist)
+        artist.save_lyrics(fileName)
+    elif songName != None and artist == None:
+        song = genius.search_song(songName)
+        print(song.lyrics)
+    elif songName != None and artist != None:
+        song = genius.search_song(songName, artist)
+        print(song.lyrics)
+    else:
+        print("Atleast one of artist and song needs to be passed. Use -s <song> -a <artist> -f <filename>")
+
+if __name__ == '__main__':
+    main()
